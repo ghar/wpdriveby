6d390ef3d1625b8c3ef250ad62a11df1372ec55e
ghar <harkin@gmail.com>
2016-04-09 20:27:52 +0100

    Update wpdriveby.sh

--- wpdriveby.sh
+++ wpdriveby.sh
@@ -1,38 +1,49 @@
-# Clean-up
-rm -f firstgrep.txt > /dev/null 2>&1
-rm -f iplist.txt > /dev/null 2>&1
-rm -f blocklist.txt > /dev/null 2>&1
 clear
+cat << "EOF"
+
+
+ _ _ _ ___ _| |___|_|_ _ ___| |_ _ _
+| | | | . | . |  _| | | | -_| . | | |
+|_____|  _|___|_| |_|\_/|___|___|_  | v 2.0
+      |_| "Hands off, hackers!" |___|
+
+
+EOF
 
 oneHourAgo=$(date --date '1 hour ago' +'%Y:%H')
 now=$(date +'%Y:%H')
 logDir="/home*/*/access-logs/*"
 
-cat << "EOF"
+function cleanup() {
+  rm -f firstgrep.txt > /dev/null 2>&1
+  rm -f iplist.txt > /dev/null 2>&1
+  rm -f blocklist.txt > /dev/null 2>&1
+}
 
-  
- _ _ _ ___ _| |___|_|_ _ ___| |_ _ _ 
-| | | | . | . |  _| | | | -_| . | | |
-|_____|  _|___|_| |_|\_/|___|___|_  | v 2.0
-      |_| "Hands off, hackers!" |___|	  
+function wplogin_find() {
+  echo -e "\nTrawling access-logs for wp-login.php..."
+  grep wp-login.php $logDir | awk '{print $1}' | sort -n | uniq -c | sort -n | \
+  tail -50 | awk '{if($1>100)print "Hits:",$1, "IP:",$2}' | tee -a "firstgrep.txt"
+}
 
+function xmlrpc_find()  {
+  echo -e "\nTrawling access-logs for xmlrpc.php hits..."
+  grep ''$hourago'\|'$now'' $logDir | grep 'xmlrpc.php' | awk '{print $1}' | sort -n | \
+  uniq -c | sort -n | tail -50 | awk '{if($1>100)print "Hits:",$1, "IP:",$2}' | tee -a "firstgrep.txt"
+}
 
-EOF
+cleanup
 
 # Prompt user and grep logs accordingly
 read -p $'Search for hits on:\n[W] wp-login.php\n[X] xmlrpc.php\n[Q] Quit\n> ' -n 1 answer
 echo -e "\n"
 if [[ $answer =~ ^[Ww]$ ]]; then
-    echo -e "\nTrawling access-logs for wp-login.php..."
-    grep wp-login.php $logDir | awk '{print $1}' | sort -n | uniq -c | sort -n | \
-    tail -50 | awk '{if($1>100)print "Hits:",$1, "IP:",$2}' | tee -a "firstgrep.txt"
+  wplogin_find
 elif [[ $answer =~ ^[Xx]$ ]]; then
-    echo -e "\nTrawling access-logs for xmlrpc.php hits..."
-    grep ''$hourago'\|'$now'' $logDir | grep 'xmlrpc.php' | awk '{print $1}' | sort -n | \
-    uniq -c | sort -n | tail -50 | awk '{if($1>100)print "Hits:",$1, "IP:",$2}' | tee -a "firstgrep.txt"
+  xmlrpc_find
 else
-    echo -e "\nExited wpdriveby.sh"
-    exit 1
+  echo -e "\nExited wpdriveby.sh"
+  exit 1
 fi
 
 # Show user IPs and Hits total. Ask to deny. If not, remove firstgrep.txt and exit.
@@ -41,9 +52,9 @@ read -p $'Continue and deny IP addresses?\n[Y] Yes\n[N] No\n> ' -n 1 -r
 echo -e "\n"
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
-    rm -f firstgrep.txt
-    echo -e "\nExited wpdriveby.sh"
-    exit 1
+  rm -f firstgrep.txt
+  echo -e "\nExited wpdriveby.sh"
+  exit 1
 fi
 
 # extract IPs only
@@ -52,7 +63,7 @@ grep "Hits:" firstgrep.txt | awk '{print $4}' | tac | egrep -o '([0-9]+\.){3}[0-
 # add csf deny to IPs
 while read line
 do
-    echo "csf -d $line" >> blocklist.txt
+  echo "csf -d $line" >> blocklist.txt
 done < iplist.txt
 
 # Block
@@ -60,9 +71,5 @@ echo "exit" >> blocklist.txt
 chmod u+x blocklist.txt
 sh blocklist.txt
 
-# Clean-up
-rm -f firstgrep.txt
-rm -f iplist.txt
-rm -f blocklist.txt
-
+cleanup
 echo -e "\nAll Done"
